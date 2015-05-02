15e3b2e4f38784f6caf5f93ca81773de69263698
Jamie Nicholls <jamie.n@hotmail.co.nz>
2015-05-02 15:29:06 +1200

    added test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitTest
+//
+//  Created by Jamie Nicholls on 2/05/15.
+//  Copyright (c) 2015 Jamie Nicholls. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitTest
+//
+//  Created by Jamie Nicholls on 2/05/15.
+//  Copyright (c) 2015 Jamie Nicholls. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
